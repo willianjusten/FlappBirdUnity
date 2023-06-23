c48e0784ddf783aede877ee20febb310d5570f0f
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-23 15:02:00 -0300

    Add Play/Pause logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
     private int score;
 
+    private void Awake() {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play() {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    } 
+
     public void IncreaseScore() {
-        Debug.Log("Score");
         score++;
+        scoreText.text = score.ToString();
     }
 
     public void GameOver() {
-        Debug.Log("GameOver");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 }
