a686c05468cac21a07ef061234821da9c3ad79d6
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-23 15:02:00 -0300

    Add Player gravity logic and sprite animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Vector3 direction;
+
+    public float gravity = -9.8f;
+
+    public float strength = 5f;
+    
+    public Sprite[] sprites;
+
+    private int spriteIndex;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            direction = Vector3.up * strength;
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite() {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length) {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+}
